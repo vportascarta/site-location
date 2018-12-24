9b1751672b85b0f54364edc3048096675a1f3c8b
Vincent PORTA-SCARTA <vincent.porta@outlook.fr>
2018-12-24 14:13:57 +0100

    Update function and add mail api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -33,20 +33,47 @@ interface InfoMail {
 // Init App & Auth
 admin.initializeApp(functions.config().firebase);
 const db = admin.database();
+
 const functionsOauthClient = new OAuth2Client(credentials.client_id, credentials.secret, credentials.redirect_url);
 let oauthTokens = null;
+
 const DB_TOKEN_PATH = '/api_tokens';
 const DB_DISPO_PATH = '/dispo';
 const DB_PRICES_PATH = '/prices';
 
+const mailConfig = {
+    host: credentials.mail_server,
+    port: 465,
+    secure: true,
+    auth: {
+        user: credentials.mail_user,
+        pass: credentials.mail_password
+    }
+};
+
 // Visit the URL for this Function to request tokens
 exports.authgoogleapi = functions.https.onRequest((req, res) => {
-    res.set('Cache-Control', 'private, max-age=0, s-maxage=0');
-    res.redirect(functionsOauthClient.generateAuthUrl({
-        access_type: 'offline',
-        scope: credentials.scopes,
-        prompt: 'consent',
-    }));
+    db.ref(DB_TOKEN_PATH).once('value')
+        .then((snapshot) => {
+            const data = snapshot.val();
+
+            if (data === undefined) {
+                res.set('Cache-Control', 'private, max-age=0, s-maxage=0');
+                res.redirect(functionsOauthClient.generateAuthUrl({
+                    access_type: 'offline',
+                    scope: credentials.scopes,
+                    prompt: 'consent',
+                }));
+                return;
+            } else {
+                res.status(403).send("Already connected");
+                return;
+            }
+        })
+        .catch((err) => {
+            res.status(500).send("An error as occur : " + err);
+            return;
+        })
 });
 
 // After you grant access, you will be redirected to the URL for this Function
@@ -57,12 +84,13 @@ exports.oauthcallback = functions.https.onRequest((req, res) => {
     functionsOauthClient.getToken(code, (err, tokens) => {
         // Now tokens contains an access_token and an optional refresh_token. Save them.
         if (err) {
-            return res.status(400).send(err);
+            res.status(500).send("An error as occur : " + err);
+            return;
         }
         return admin.database().ref(DB_TOKEN_PATH).set(tokens)
             .then(() => {
-                return res.status(200).send('App successfully configured with new Credentials. '
-                    + 'You can now close this page.');
+                res.status(200).send('App successfully configured with new Credentials. You can now close this page.');
+                return;
             });
     });
 });
@@ -89,16 +117,18 @@ function readDataFromCalandar() {
     };
 
     return new Promise((resolve, reject) => {
-        return getAuthorizedClient().then((auth) => {
-            const calendar = google.calendar({version: 'v3', auth});
-            return calendar.events.list(config, (err, response) => {
-                if (err) {
-                    console.log(`The API returned an error: ${err}`);
-                    reject(err);
-                }
-                resolve(response.data.items);
-            });
-        });
+        return getAuthorizedClient()
+            .then((auth) => {
+                const calendar = google.calendar({version: 'v3', auth});
+                return calendar.events.list(config, (err, response) => {
+                    if (err) {
+                        console.log(`The API returned an error: ${err}`);
+                        reject(err);
+                    }
+                    resolve(response.data.items);
+                });
+            })
+            .catch((err) => reject(err));
     });
 }
 
@@ -110,60 +140,70 @@ function readDataFromSheet(sheet_name: string) {
     };
 
     return new Promise((resolve, reject) => {
-        return getAuthorizedClient().then((auth) => {
-            const sheets = google.sheets({version: 'v4', auth});
-            return sheets.spreadsheets.values.get(config, (err, response) => {
-                if (err) {
-                    console.log(`The API returned an error: ${err}`);
-                    reject(err);
-                }
-                resolve(response.data.values);
-            });
-        });
+        return getAuthorizedClient()
+            .then((auth) => {
+                const sheets = google.sheets({version: 'v4', auth});
+                return sheets.spreadsheets.values.get(config, (err, response) => {
+                    if (err) {
+                        console.log(`The API returned an error: ${err}`);
+                        reject(err);
+                    }
+                    resolve(response.data.values);
+                });
+            })
+            .catch((err) => reject(err));
     });
 }
 
 function fetchIndisponibility(location_name: Places): Promise<void> {
-    return readDataFromCalandar().then((data: [any]) => {
-        const indispos: Period[] = [];
+    return new Promise((resolve, reject) => {
+        readDataFromCalandar()
+            .then((data: [any]) => {
+                const indispos: Period[] = [];
 
-        for (const item of data) {
-            if (item.summary.toLocaleLowerCase() === location_name) {
-                indispos.push({
-                    start: item.start.date,
-                    end: item.end.date,
-                })
-            }
-        }
+                for (const item of data) {
+                    if (item.summary.toLocaleLowerCase() === location_name) {
+                        indispos.push({
+                            start: item.start.date,
+                            end: item.end.date,
+                        })
+                    }
+                }
 
-        const res = {
-            updatedAt: Date.now(),
-            indispos
-        };
+                const res = {
+                    updatedAt: Date.now(),
+                    indispos
+                };
 
-        return db.ref(DB_DISPO_PATH + `/${location_name}`).set(res);
+                resolve(db.ref(DB_DISPO_PATH + `/${location_name}`).set(res));
+            })
+            .catch((err) => reject(err))
     });
 }
 
 function fetchPricePeriods(location_name: string): Promise<void> {
-    return readDataFromSheet(location_name).then((data: [any]) => {
-        const prices: PricePeriod[] = [];
+    return new Promise((resolve, reject) => {
+        readDataFromSheet(location_name)
+            .then((data: [any]) => {
+                const prices: PricePeriod[] = [];
 
-        for (const item of data) {
-            prices.push({
-                start: item[0],
-                end: item[1],
-                price: item[2],
+                for (const item of data) {
+                    prices.push({
+                        start: item[0],
+                        end: item[1],
+                        price: item[2],
+                    })
+                }
+
+                const res = {
+                    updatedAt: Date.now(),
+                    prices
+                };
+
+                resolve(db.ref(DB_PRICES_PATH + `/${location_name}`).set(res));
             })
-        }
-
-        const res = {
-            updatedAt: Date.now(),
-            prices
-        };
-
-        return db.ref(DB_PRICES_PATH + `/${location_name}`).set(res);
-    });
+            .catch((err) => reject(err))
+    })
 }
 
 // Export functions
@@ -174,6 +214,7 @@ exports.indisponibility = functions.https.onRequest((req, res) => {
     // Check for a correct request
     if (req.method !== "GET" || location_name === undefined) {
         res.status(400).send('Please send a correct request');
+        return;
     }
 
     // Get database db
@@ -182,10 +223,16 @@ exports.indisponibility = functions.https.onRequest((req, res) => {
         .then((snapshot) => {
             const updatedAt: number = snapshot.val();
             if (Date.now() > updatedAt + 24 * 60 * 60 * 1000) {
-                fetchIndisponibility(location_name).then(() => sendRes()).catch()
+                fetchIndisponibility(location_name).then(() => sendRes()).catch();
+                return;
             } else {
-                sendRes()
+                sendRes();
+                return;
             }
+        })
+        .catch((err) => {
+            res.status(500).send("An error as occur : " + err);
+            return;
         });
 
     function sendRes() {
@@ -194,18 +241,23 @@ exports.indisponibility = functions.https.onRequest((req, res) => {
             .then((snapshot_) => {
                 const indispos: Period[] = snapshot_.val();
                 res.status(200).send(indispos);
+                return;
             })
-            .catch(() => res.status(400).send("Error"))
+            .catch((err) => {
+                res.status(500).send("An error as occur : " + err);
+                return;
+            });
     }
 });
 
-exports.pricePeriods = functions.https.onRequest((req, res) => {
+exports.priceperiods = functions.https.onRequest((req, res) => {
     // Grab the location name parameter.
     const location_name: Places = req.query.location;
 
     // Check for a correct request
     if (req.method !== "GET" || location_name === undefined) {
         res.status(400).send('Please send a correct request');
+        return;
     }
 
     // Get database db
@@ -214,10 +266,16 @@ exports.pricePeriods = functions.https.onRequest((req, res) => {
         .then((snapshot) => {
             const updatedAt: number = snapshot.val();
             if (Date.now() > updatedAt + 24 * 60 * 60 * 1000) {
-                fetchPricePeriods(location_name).then(() => sendRes()).catch()
+                fetchPricePeriods(location_name).then(() => sendRes()).catch();
+                return;
             } else {
-                sendRes()
+                sendRes();
+                return;
             }
+        })
+        .catch((err) => {
+            res.status(500).send("An error as occur : " + err);
+            return;
         });
 
     function sendRes() {
@@ -226,32 +284,47 @@ exports.pricePeriods = functions.https.onRequest((req, res) => {
             .then((snapshot_) => {
                 const prices: PricePeriod[] = snapshot_.val();
                 res.status(200).send(prices);
+                return;
             })
-            .catch(() => res.status(400).send("Error"))
+            .catch((err) => {
+                res.status(500).send("An error as occur : " + err);
+                return;
+            });
     }
 });
 
-/*
-exports.sendEmail = functions.https.onRequest((req, res) => {
-    // Check for POST request
-    if (req.method !== "POST") {
-        res.status(400).send('Please send a POST request');
-        
+exports.sendemail = functions.https.onRequest((req, res) => {
+    // Check for a correct request
+    if (req.method !== "POST" || req.body === undefined) {
+        res.status(400).send('Please send a correct request');
+        return;
     }
 
-    const poolConfig = {
-        pool: true,
-        host: 'smtp.orange.fr',
-        port: 465,
-        secure: true, // use TLS
-        auth: {
-            user: 'username',
-            pass: 'password'
-        }
-    };
+    const transporter = nodemailer.createTransport(mailConfig);
 
     // Extract data
-    const data: InfoMail = req.body
+    const data: InfoMail = req.body;
 
+    const message = {
+        from: credentials.mail_user,
+        to: credentials.mail_dest,
+        subject: `Demande de renseignement pour ${data.place}`,
+        text:
+            `Vous avez reçu une demande de renseignement pour le logement ${data.place} :
+- Nom : ${data.name}
+- Dates : Du ${data.dates.start.toLocaleDateString("fr-FR")} au ${data.dates.end.toLocaleDateString("fr-FR")}
+- Email pour répondre : ${data.senderMail}
+
+${data.message}`
+    };
+
+    transporter.sendMail(message)
+        .then(() => {
+            res.status(200).send('Message sent');
+            return;
+        })
+        .catch((err) => {
+            res.status(500).send("An error as occur : " + err);
+            return;
+        });
 });
-*/
